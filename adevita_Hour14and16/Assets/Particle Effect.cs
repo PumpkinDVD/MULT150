360219d6967ffceaac7c0d1178726e0103ae8938
PumpkinDVD <alfredman29@gmail.com>
2025-11-21 10:21:54 -0800

    Hour 16

---
+++ adevita_Hour14and16/Assets/Particle Effect.cs
@@ -0,0 +1,6 @@
+/*  1. Made the colors a gradient
+ *  2. Made the size change over time
+ *  3. Change the rate over time to 200
+ *  4. Keyed the rate over time so that it shoots out in bursts
+ *  5. Made the particles shoot out in a sphere shape
+ */
