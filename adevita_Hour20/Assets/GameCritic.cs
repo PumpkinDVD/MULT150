b5dd4fbd48a423866313b82c8a8f6c21bf97ae41
PumpkinDVD <alfredman29@gmail.com>
2025-12-12 10:30:22 -0800

    Hour 20

---
+++ adevita_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,14 @@
+/* 1. I think the game is on the easier side. Not too difficult to avoid obstacles. */
+
+/* 2. I guess more obstacles would make it harder. */
+
+/* 3. Sound effects might add some appeal. */
+
+/* 4. Dodging obstacles is slightly fun. */
+
+/* 5. Collecting powerups is tedious. */
+
+
+/* Made a rusty saw obstacle. https://www.textures4photoshop.com/tex/grunge-and-rust/seamless-rusty-metal-texture.aspx */
+
+/* Made the powerup a plus. Gave the particles a green-red-yellow gradient.The start size is also random. */
