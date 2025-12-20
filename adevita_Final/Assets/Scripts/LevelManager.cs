9e0232aa77cf85d7020a64528e63b6082ba7706d
PumpkinDVD <alfredman29@gmail.com>
2025-12-19 18:37:58 -0800

    Final

---
+++ adevita_Final/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	public string sceneToLoad = "Main";
+
+
+	public void LoadGame ()
+	{
+		SceneManager.LoadScene(sceneToLoad);
+	}
+}
---
+++ adevita_Final/Assets/Scripts/Modifications.cs
@@ -0,0 +1,59 @@
+﻿public class Modifications
+{/*
+    ░▒▓████████▓▒░▒▓█▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓██████▓▒░ ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░ */
+
+
+    // Choose 1 of the games below and list modifications 
+    // Delete the other 2 games
+
+
+    /*  ================================================================
+        GAUNTLET RUNNER
+        ================================================================
+        1 - directional light (colour, intensity, etc)
+        modifications: Changed direction, color to neon-ish blue, and intensity to 2
+
+        2 - fog
+        modifications: Added light red fog
+
+        3 - create & apply skybox and customise it (atmosphere thickness, sky tint, exposure, etc)
+        modifications: Edited existing skybox: lowered atmosphere thickness, changed the sky tints to be muted blue and red
+        
+        OBSTACLE PREFAB
+        4 - add or modify light component (colour, range, intensity, etc)
+        modifications: white point light, intesity 3, range 12
+        
+        POWERUP PREFAB
+        5 - replace with a new complex object comprised of 3 or more shapes (different from previously made object)
+        modifications: made a clock. Particle system: white, , 
+
+        6 - use gameobject hierarchy to keep the powerup prefab organised
+        [no record necessary]
+
+        7 - apply at least 2 new textures
+        (add source url or package name)
+        texture source 1: https://www.deviantart.com/agf81/art/Clock-Face-2-169291889
+        texture source 2: http://istockphoto.com/search/2/image-film?phrase=shiny+black+texture&tracked_gsrp_landing=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fshiny-black-texture
+
+        8 - add particle system component and customise at least 3 parameters
+        parameter 1: color: white green gradient 
+        parameter 2: start lifetime 2, start speed 3, start size 0.3
+        parameter 3: donut shape, radius 0.2
+
+        WALL
+        9 - convert a wall to prefab, create a variant, and use the variant to replace the other wall in the scene
+        [no record necessary]
+
+        10 - modify wall prefab material tint so that the 2 walls are different in appearance or colour
+        [no record necessary]
+     
+     */
+
+
+}
